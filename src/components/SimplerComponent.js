05ea41a3a3a3a0f7bd4d7e1ef4bf06a0c0a500d2
camryn williams <williams.camryn.e@gmail.com>
2021-04-28 16:10:06 -0400

    completed assignment

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,13 @@
 // Code SimplerComponent Here
+
+import React, { Component } from 'react'
+
+// export default class SimplerComponent extends Component {
+//     render() {
+//         const SimplerComponent = (props) => <div onClick={props.handleClick}>I am just happy</div>
+//     }
+//     //const SimplerComponent = (props) => <div onClick={props.handleClick}>I am just happy</div>
+// }
+
+const SimplerComponent = (props) => <div onClick={props.handleClick}>I am just happy</div>
+export default SimplerComponent
